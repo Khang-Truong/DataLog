27aba5aa9cc54b1ef0ea5122e2abd4f727ba655c
Summer <gamits@srudent.douglascollege.ca>
2022-10-28 23:09:07 -0700

    adding revenue.db, wastagedb and api's for revenue and wastage

--- backend/model.py
+++ backend/model.py
@@ -4,3 +4,18 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     title: str
     description: str
+
+
+class DailyRevenue(BaseModel):
+    Date: str
+    PredictedRevenue: int
+    Flag_latest: str
+
+
+#didn't really use, for noww
+class Wastage(BaseModel):
+    Category: str
+    Product_Name: str
+    Quantity: int
+    Date: str
+
