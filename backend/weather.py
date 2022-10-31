32e8fb6538827dad4d9413fb47a92c63e71486e0
Summer <gamits@student.douglascollege.ca>
2022-10-31 16:19:04 -0700

    update in weather api

--- backend/weather.py
+++ backend/weather.py
@@ -1,6 +1,7 @@
 import requests
 from tkinter import *
 import math
+from model import WeatherForecast
 
 
 
@@ -8,10 +9,16 @@ def get_weather():
     city_name = "Vancouver,CA"
     api_key = '26e3ab58c982deb6571267759cd689e1'
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={city_name}&appid={api_key}"
+     
+    response = requests.get(url).json()["list"]
     
-    response = requests.get(url).json()
-    
-    
-    
-    return response
+    #getting only one weather update per day
+
+    response_list = []
+
+    for x in range(4,40,8):
+        response_list.append(response[x])
+
+    return response_list
+
     
